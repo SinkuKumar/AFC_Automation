ae6e373cd13416b08127a805e6074af3b66d3187
Mallik <niladrimallik.p@hq.graphxsys.com>
2025-03-05 16:01:02 +0530

    cnt_27_report_download script complete, main.py calling cnt_27_report_download script

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import os
+import sys
+from pdb import run
 import time
+from datetime import datetime
 import logging
 import traceback
 from dotenv import load_dotenv
@@ -7,24 +10,41 @@ from utils.selenium_driver import SeleniumDriver
 from utils.experity_base import ExperityBase
 from utils.file_folder import FileOperations
 
-def complete_report(report_name: str, download_dir: str, browser: str, username: str):
-    '''
+def complete_report(report_info: dict,
+                    browser: str,
+                    user_info: dict,
+                    logger_instance: logging.getLogger
+                    ):
+    """
     Function to complete login and download one particular report.
 
-    Args:
-    :param report_name: Name of the report to be downloaded
-    :type report_name: str
-
-    :param download_dir: directory where the file needs to be downloaded
-    :type download_dir: str
+    :param report_info: dictionary, containing the details of the report.
+        ```
+        report_info = {'download_dir' : '', 'report_name' : ''}
+        ```
+    :type report_info: dict
 
     :param browser: the desired browser to open ['chrome', 'firefox', 'edge']
     :type browser: str
 
-    :param username: the login username of the client
-    :type username Str
-    '''
+    :param user_info: dictionary containing the username and password
+        ```
+            user_info = {'username' : '', 'password' : ''}
+        ```
+    :type user_info: dict
+
+    :logger_instance: the instance of the logger class
+    :type logger_instance: logging.getLogger
+
+    :param run_sp: `True` if SP has to be run, else `False`, default `True`
+    :type run_sp: True
+
+    Returns None
+    """
     try:
+        download_dir = report_info['download_dir']
+        report_name = report_info['report_name']
+
         # TODO: Remove this in production, create this in top layer.
         if not os.path.exists(download_dir):
             os.makedirs(download_dir)
@@ -36,41 +56,32 @@ def complete_report(report_name: str, download_dir: str, browser: str, username:
         experity = ExperityBase(driver)
         file_operation = FileOperations()
 
-        load_dotenv()
-
-        logger = logging.getLogger("my_logger")
-        logger.setLevel(logging.DEBUG)
-
-        console_handler = logging.StreamHandler()
-        console_handler.setLevel(logging.DEBUG)
-        log_format = f"{{asctime}} : {__name__} : {{levelname}} : {{message}}"
-        console_formatter = logging.Formatter(
-            fmt = log_format,
-            style = "{"
-        )
-        console_handler.setFormatter(console_formatter)
-        logger.addHandler(console_handler)
-
-        username = os.getenv('EXP_USERNAME')
-        password = os.getenv('PASSWORD')
-
         experity.open_portal(experity_url)
-        logger.info("Opened portal")
+        logger_instance.info("Opened portal")
+
+        # TODO: Maybe remove this 6 lines in production
+        username = user_info["username"]
+        password = user_info['password']
+        client_id = user_info['client_id']
+
+        print(username)
+        print(password)
+        print(client_id)
 
         experity.login(username, password)
-        logger.info("Logged in")
+        logger_instance.info("Logged in")
 
         experity_version = experity.get_portal_url()
-        logger.info("Got portal URL")
+        logger_instance.info("Got portal URL")
 
         experity.navigate_to(experity_url, experity_version, "Reports")
-        logger.info("Navigate to method called")
+        logger_instance.info("Navigate to method called")
 
         experity.search_and_select_report("CNT_27")
-        logger.info("Search and select report method called")
+        logger_instance.info("Search and select report method called")
 
         experity.select_report_date_range('02/02/2025', '02/02/2025')
-        logger.info("Date report range method called")
+        logger_instance.info("Date report range method called")
 
         experity.select_logbook_status(['All'])
 
@@ -79,18 +90,55 @@ def complete_report(report_name: str, download_dir: str, browser: str, username:
 
         # Uncheck all check all option ??
         experity.run_report()
-        logger.info("Run report method called")
+        logger_instance.info("Run report method called")
         experity.download_report('CSV')
         file_operation.wait_for_download(report_name, download_dir)
-        logger.info("Download report method called")
-
-        # Implement the stored procedure partx
-        
+        logger_instance.info("Download report method called")
 
     except Exception as e:
-        logger.info("Error occurred: %s %s", type(e).__name__, e)
+        logger_instance.info("Error occurred: %s %s", type(e).__name__, e)
+
+def get_logger_name(log_name):
+    """
+    Method to define the logger for this module.
+
+    :param log_name: name of the logger
+
+    :returns: A logger instance
+    :rtype: logger.getLogger()
+    """
+    logger = logging.getLogger(log_name)
+    logger.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    log_format = f"{{asctime}} : {__name__} : {{levelname}} : {{message}}"
+    console_formatter = logging.Formatter(
+        fmt = log_format,
+        style = "{"
+    )
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
+    return logger
 
 
+if __name__ == "__main__":
+    load_dotenv('.env')
 
-report_name = "CNT_27"
-download_dir = os.path.join(os.getcwd(), 'Downloaded Reports')
+    today = datetime.now().strftime("%Y_%m_%d")
+    report_info ={}
+    report_info['report_name'] = "CNT_27"
+    report_info['download_dir'] = os.path.join(os.getcwd(), 'Downloaded Reports', today)
+    browser = "chrome"
+
+    user_info = {}
+    user_info['client_id'] = os.getenv("CLIENT_ID")
+    user_info["username"] = os.getenv('EXP_USERNAME')
+    user_info["password"] = os.getenv('PASSWORD')
+
+    logger_inst = get_logger_name("main")
+
+    complete_report(report_info = report_info,
+                    browser = browser, user_info=user_info,
+                    logger_instance=logger_inst
+                    )

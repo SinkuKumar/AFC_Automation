fba39e520ed68caf6fec50f495601894cae72f06
Mallik <niladrimallik.p@hq.graphxsys.com>
2025-03-13 20:29:29 +0530

    feat: add new report functions for LAB_01, XRY_03, CHT_02, MED_01, PER_2, PAT_20, CCR_02, and CCR_03

--- download_reports_sequential.py
+++ download_reports_sequential.py
@@ -42,6 +42,46 @@ fin_18_report_name = 'FIN_18'
 fin_18_from_date = cnt_27_from_date
 fin_18_to_date = cnt_27_to_date
 
+pay_41_report_name = 'PAY_41'
+pay_41_from_date = cnt_27_from_date
+pay_41_to_date = cnt_27_to_date
+
+pat_2_report_name = 'PAT_2'
+pat_2_from_date = cnt_27_from_date
+pat_2_to_date = cnt_27_to_date
+
+lab_01_report_name = 'PAT_2'
+lab_01_from_date = cnt_27_from_date
+lab_01_to_date = cnt_27_to_date
+
+xry_03_report_name = 'XRY_03'
+xry_03_from_date = cnt_27_from_date
+xry_03_to_date = cnt_27_to_date
+
+cht_02_report_name = 'XRY_03'
+cht_02_from_date = cnt_27_from_date
+cht_02_to_date = cnt_27_to_date
+
+med_01_report_name = 'MED_01'
+med_01_from_date = cnt_27_from_date
+med_01_to_date = cnt_27_to_date
+
+per_02_report_name = 'PER_2'
+per_02_from_date = cnt_27_from_date
+per_02_to_date = cnt_27_to_date
+
+pat_20_report_name = 'PAT_20'
+pat_20_from_date = cnt_27_from_date
+pat_20_to_date = cnt_27_to_date
+
+ccr_02_report_name = 'CCR_02'
+ccr_02_from_date = cnt_27_from_date
+ccr_02_to_date = cnt_27_to_date
+
+ccr_03_report_name = 'CCR_03'
+ccr_03_from_date = cnt_27_from_date
+ccr_03_to_date = cnt_27_to_date
+
 print(f'Running for Client {CLIENT_ID}')
 
 DOWNLOAD_DIRECTORY = os.path.join(os.getcwd(), 'Temp_Downloads')
@@ -72,6 +112,18 @@ reports.cnt_19(cnt_19_report_name, cnt_19_from_date, cnt_19_to_date)
 reports.fin_25(fin_25_report_name, fin_25_from_date, fin_25_to_date)
 reports.adj_11(adj_11_report_name, adj_11_from_date, adj_11_to_date)
 reports.fin_18(fin_18_report_name, fin_18_from_date, fin_18_to_date)
+reports.pay_41(pay_41_report_name, pay_41_from_date, pay_41_to_date)
+reports.pat_2(pat_2_report_name, pat_2_from_date, pat_2_to_date)
+
+reports.lab_01(lab_01_report_name, lab_01_from_date, lab_01_to_date)
+reports.xry_03(xry_03_report_name, xry_03_from_date, xry_03_to_date)
+reports.cht_02(cht_02_report_name, cht_02_from_date, cht_02_to_date)
+reports.med_01(med_01_report_name, med_01_from_date, med_01_to_date)
+reports.per_02(per_02_report_name, per_02_from_date, per_02_to_date)
+reports.pat_20(pat_20_report_name, pat_20_from_date, pat_20_to_date)
+
+reports.ccr_02(ccr_02_report_name, ccr_02_from_date, ccr_02_to_date)
+reports.ccr_03(ccr_03_report_name, ccr_03_from_date, ccr_03_to_date)
 
 time.sleep(1000)
 experity.logout()
